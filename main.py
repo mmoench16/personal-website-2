931387d011596fe283815dd540eb59c95b037df8
mmoench16 <m.moench16@gmail.com>
2025-09-22 20:15:57 +0100

    Work on Contact form

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import ContactForm
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
 
 @app.context_processor
 def inject_year():
@@ -15,9 +30,22 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+
+        msg = Message(f'New message from {name}', sender='your@email.com', recipients=['recipient@example.com'])
+        msg.body = f'From: {name} <{email}>\n\n{message}'
+        mail.send(msg)
+
+        flash('Your message has been sent!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template("contact.html", form=form)
 
 @app.route("/blog")
 def blog():
